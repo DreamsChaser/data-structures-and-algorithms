5919eac16a33b1bf5ac11f40848508a290f06863
leon <18661230709@163.com>
2018-01-05 17:27:15 +0800

    select sort

---
+++ src/main/java/cn/com/leon/sort/SelectSort.java
@@ -0,0 +1,21 @@
+package cn.com.leon.sort;
+
+/**
+ * @author leon
+ *         选择排序
+ */
+public class SelectSort {
+
+    /**
+     * 选择排序
+     *
+     * @param arr
+     */
+    public static void selectSort(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
