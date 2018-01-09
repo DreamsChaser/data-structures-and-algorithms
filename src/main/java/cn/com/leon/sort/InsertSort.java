bfe20cd556f142281843518067f6a3f5337743eb
leon <18661230709@163.com>
2018-01-09 17:36:33 +0800

    select sort

---
+++ src/main/java/cn/com/leon/sort/InsertSort.java
@@ -0,0 +1,27 @@
+package cn.com.leon.sort;
+
+/**
+ * 插入排序
+ *
+ * @author leon
+ */
+public class InsertSort {
+
+    /**
+     * 插入排序
+     *
+     * @param arr
+     */
+    public static void insertSort(int[] arr) {
+        int temp = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j >= 0; j--) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
