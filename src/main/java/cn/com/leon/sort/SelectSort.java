f82926587df59c8d2e9cdc899638844ac28a970a
leon <18661230709@163.com>
2018-01-09 15:59:15 +0800

    select sort

--- src/main/java/cn/com/leon/sort/SelectSort.java
+++ src/main/java/cn/com/leon/sort/SelectSort.java
@@ -1,5 +1,7 @@
 package cn.com.leon.sort;
 
+import java.util.Arrays;
+
 /**
  * @author leon
  *         选择排序
@@ -12,10 +14,23 @@ public class SelectSort {
      * @param arr
      */
     public static void selectSort(int[] arr) {
-
+        int temp = 0;
+        int min = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    temp = arr[min];
+                    arr[min] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            min++;
+        }
     }
 
     public static void main(String[] args) {
-
+        int[] source = {21, 12, 3, 11, 9, 37, 10, 99, 14, 2, 32, 123, 21};
+        SelectSort.selectSort(source);
+        System.out.println(Arrays.toString(source));
     }
 }
