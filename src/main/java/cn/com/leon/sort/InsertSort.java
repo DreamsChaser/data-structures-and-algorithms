f968ea577d02d4987d3775395d740890154f2f7b
leon <18661230709@163.com>
2018-01-11 10:25:38 +0800

    insert sort

--- src/main/java/cn/com/leon/sort/InsertSort.java
+++ src/main/java/cn/com/leon/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package cn.com.leon.sort;
 
+import java.util.Arrays;
+
 /**
  * 插入排序
  *
@@ -10,18 +12,24 @@ public class InsertSort {
     /**
      * 插入排序
      *
-     * @param arr
+     * @param arr 数组
      */
     public static void insertSort(int[] arr) {
-        int temp = 0;
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j >= 0; j--) {
-
+        int i, j;
+        for (i = 1; i < arr.length; i++) {
+            int target = arr[i];
+            j = i;
+            while (j > 0 && arr[j - 1] > target) {
+                arr[j] = arr[j - 1];
+                j--;
             }
+            arr[j] = target;
         }
     }
 
     public static void main(String[] args) {
-
+        int[] source = {1, 21, 12, 3, 11, 9, 37, 10, 99, 14, 2, 32, 123, 21};
+        insertSort(source);
+        System.out.println(Arrays.toString(source));
     }
 }
